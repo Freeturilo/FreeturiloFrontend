6c6dc3d3fe22afd71007f5e01c05856ad2f354d1
Mikołaj Terzyk <01142218@pw.edu.pl>
2022-01-08 21:53:56 +0100

    Added Javadoc documentation for freeturilo packages

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.freeturilo.activities"},{"l":"com.example.freeturilo.connection"},{"l":"com.example.freeturilo.core"},{"l":"com.example.freeturilo.dialogs"},{"l":"com.example.freeturilo.json"},{"l":"com.example.freeturilo.misc"},{"l":"com.example.freeturilo.storage"}]
